a0b1a7cd523a089103826b0e709b94b29d7d5bf4
alexkimoni <52629501+alexkimoni@users.noreply.github.com>
2025-04-30 10:12:42 -0600

    second commit

--- Demo2/src/rrVer/MyFirstVer.java
+++ Demo2/src/rrVer/MyFirstVer.java
@@ -4,6 +4,10 @@ public class MyFirstVer {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// test info
+		int randomVar;
+		
+		randomVar = randomVar ^ 2;
 
 	}
 
